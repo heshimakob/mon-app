ab4b8446375a6dc2838e6556387270381d44eaef
heshimakob <heshimakob@gmail.com>
2021-02-08 14:12:16 +0100

    salut

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Nav from './Nav';
+import Contact from './Contact';
+import About from './About';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Nav/>
+      <switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+      </switch>
+    </div>
+     </Router>
+  );
+}
+const Home = () =>(
+  <div>
+    <h1>Page d'accueil</h1>
+  </div>
+)
+
+export default App;
---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Contact() {
+  return (
+    <div className="Contact">
+        <h1>Page Contact</h1>
+    </div>
+  );
+}
+
+export default Contact;
